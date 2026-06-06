b27f4c6e3e46c78af80ff4e12e662a04ae8fcd6e
TanishSharma151 <tanishrajsharma151@gmail.com>
2026-06-06 15:50:05 +0530

    Production ready

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3001',
+    origin:
+      process.env.FRONTEND_URL,
     credentials: true,
   });
 
